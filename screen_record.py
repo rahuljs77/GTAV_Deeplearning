7389d024febf707d0910c6d9cf6dcc3656c6e981
Rahul <sjaladi@g.clemson.edu>
2020-08-27 22:37:55 -0400

    Made changes to image processing

--- screen_record.py
+++ screen_record.py
@@ -1,6 +1,9 @@
 from grabscreen import grab_screen
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import math
+import pygame
+import pyvjoy
 from getKeys import key_check
 import pyautogui
 import numpy as np
@@ -28,11 +31,22 @@ for i in range(0, 4):
     print("{}".format(k))
     time.sleep(1)
 pause = False
+
+#  Joystick Controller setup
+pygame.init()
+pygame.joystick.init()
+joystick_count = pygame.joystick.get_count()
+print(joystick_count)
+controller = pygame.joystick.Joystick(1)
+controller.init()
+name = controller.get_name()
+print("Controller Name: ", name)
+
 while True:
     if not pause:
         screen = grab_screen(region=(0, 35, 800, 630))
-        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2HLS)
-        new_img = screen/255 - 0.1
+        # screen = cv2.cvtColor(screen, cv2.COLOR_BGR2HLS)
+        # new_img = screen/255 - 0.1
         new_img = cv2.resize(new_img, (400, 300))
         cv2.imshow('screen', new_img)
 
@@ -40,23 +54,25 @@ while True:
         FPS = 1/(curr_time - prev_time)
         # print("FPS:", FPS)
         prev_time = time.time()
+        pygame.event.pump()
+        steering_angle = controller.get_axis(0) * 100
 
-        key = key_check()
-        if key:
-            if key[0] == "A":
-                steer = -10
-            else:
-                steer = 10
-        else:
-            steer = 0
+        # key = key_check()
+        # if key:
+        #     if key[0] == "A":
+        #         steer = -10
+        #     else:
+        #         steer = 10
+        # else:
+        #     steer = 0
 
         plt.show()
-        training_data.append([new_img, steer])
-        if len(training_data) % 1000 == 0:
-            print(len(training_data))
-            np.save(file, training_data)
+        training_data.append([new_img, steering_angle])
+        # if len(training_data) % 1000 == 0:
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
+            print(len(training_data))
+            np.save(file, training_data)
             cv2.destroyAllWindows()
             break
 
