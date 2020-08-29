be0caf4b1089278cb8b7fe9d0394f3f748c3dd4b
Rahul <sjaladi@g.clemson.edu>
2020-08-28 22:03:19 -0400

    test errors in data

---
+++ data_test.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+allow_pickle = True
+train_data = np.load('training_data.npy', allow_pickle=True)
+images = []
+steer = []
+for data in train_data:
+    images.append(data[0])
+    steer.append(data[1])
+    flip_image = np.fliplr(data[0])
+    images.append(flip_image)
+    steer.append(-data[1])
+
+X_train = np.array(images)
+plt.imshow(X_train[0])
+plt.show()
+print(len(X_train))
+y_train = np.array(steer)
+print(max(y_train))
