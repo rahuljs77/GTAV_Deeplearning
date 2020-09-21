ce41ab7c7237c7b1d7667bcd6d4330ceb0834269
Rahul <sjaladi@g.clemson.edu>
2020-09-20 23:31:04 -0400

    tests collected data

--- data_test.py
+++ data_test.py
@@ -3,34 +3,44 @@ import matplotlib.pyplot as plt
 import cv2
 from sklearn.utils import shuffle
 
-
 allow_pickle = True
-train_data = np.load('3pv_data.npy', allow_pickle=True)
+train_data = np.load('data/data.npy', allow_pickle=True)
+print(len(train_data))
 images = []
-steer = []
 # while True:
-for data in train_data:
+for i in range(0, 200):
+    data = train_data[i]
     source_image = data[0]
     new_image = source_image
-    new_image = cv2.resize(new_image, (400, 132))
+    width = new_image.shape[0]
+    height = new_image.shape[1]
+    new_image = cv2.resize(new_image, (width*5, height*5))
     new_image = cv2.GaussianBlur(new_image, (3, 3), 0)
-    new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2HLS)
+    new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2YUV)
     new_image = new_image/255 - 0.1
-    steering_angle = data[1]
-    if -0.1 < steering_angle < 0.1:
-        steering_angle = 0
-    steer.append(steering_angle)
-    cv2.imshow("image", new_image)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    images.append(new_image)
 
-# X_train = np.array(images)
-# print(len(X_train))
-# y_train = np.array(steer)
-# print(y_train)
+
+X_train = np.array(images, dtype="float")
+X1_train = np.array(images)
+plt.imshow(X_train[50])
+plt.show()
+print(X_train[10])
+print(X1_train[10])
+
+    # control = data[1]
+    # print(control)
+    # cv2.imshow("image", new_image)
+    # if cv2.waitKey(25) & 0xFF == ord('q'):
+    #     cv2.destroyAllWindows()
+    #     break
+
 #
-# X_train, y_train = shuffle(X_train, y_train)
-
+# x = [1, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8]
+# y = len(x)
+# indices = list(range(y))
+# np.random.shuffle(indices)
+# print(indices)
+# print(np.floor(9.7))
 
 
