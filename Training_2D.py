d509cca12edcd58a7ac4d179faffd6b667f75c2f
Rahul <sjaladi@g.clemson.edu>
2020-09-23 21:56:47 -0400

    Training the model

--- Training_2D.py
+++ Training_2D.py
@@ -11,7 +11,7 @@ from sklearn.utils import shuffle
 
 import cv2
 
-train_data = np.load('data/tf_data.npy', allow_pickle=True)
+train_data = np.load('data/tf_data1.npy', allow_pickle=True)
 
 noise = 0.05
 images = []
@@ -21,36 +21,37 @@ forward = 0
 backward = 0
 normal = 0
 print(len(train_data))
+
+
 for data in train_data:
     i = i+1
     source_image = data[0]
-    new_image = cv2.resize(source_image, (100, 100))
+    new_image = cv2.resize(source_image, (200, 66))
     new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2HLS)
     new_image = cv2.GaussianBlur(new_image, (3, 3), 0)
     new_image = new_image/255 - 0.1
     control = data[1]
 
+    # Data cleaning
     if control[1] == 0.0 or control[1] == 0.5:
-        if i % 1 == 0:
-            forward += 1
-            images.append(new_image)
-            controls.append(control)
+        forward += 1
+        images.append(new_image)
+        controls.append(control)
+
+    elif -0.3 > control[0] > 0.3 or control[1] < 0.1:  # The images with negative throttle and high steering angle are duplicated
+        backward += 2
+        copy_image = new_image - 0.05
+        if control[0] < 0:
+            copy_control = [control[0] - noise, control[1] - 0.1]
         else:
-            pass
-    elif -0.3 > control[0] > 0.3 or control[1] < 0:
-        if i % 1 == 0:
-            backward += 1
-            copy_image = new_image - 0.05
-            if control[0] < 0:
-                copy_control = [control[0] - noise, control[1]]
-            else:
-                copy_control = [control[0] + noise, control[1]]
-            images.append(new_image)
-            controls.append(control)
-            images.append(copy_image)
-            controls.append(copy_control)
+            copy_control = [control[0] + noise, control[1] - 0.1]
+
+        images.append(new_image)
+        controls.append(control)
+        images.append(copy_image)
+        controls.append(copy_control)
     else:
-        if i % 2 == 0:
+        if i % 3 == 0:
             normal += 1
             images.append(new_image)
             controls.append(control)
@@ -69,11 +70,11 @@ print("the image is being trained on {} samples".format(len(X_train)))
 epochs = 10
 batch_size = 128
 
-vgg_model = VGG16(weights='imagenet', include_top=False, input_shape=(100, 100, 3))
+vgg_model = VGG16(weights='imagenet', include_top=False, input_shape=(66, 200, 3))  # pretrained vgg16 model with last layer chopped off.
 for layer in vgg_model.layers:
     layer.trainable = False
 
-data_input = Input(shape=(100, 100, 3))
+data_input = Input(shape=(66, 200, 3))
 vgg = vgg_model(data_input)
 drop_1 = Dropout(0.2)(vgg)
 flat_1 = Flatten()(drop_1)
@@ -94,6 +95,6 @@ model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=epochs, v
 print()
 print('training complete')
 
-model.save('models/tf_model.h5')
+model.save('models/tf_model1.h5')
 print()
 print('model saved')
