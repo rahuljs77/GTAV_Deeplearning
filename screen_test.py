306fdd67197401dc5c8ecfa581f7816125c3588f
Rahul <sjaladi@g.clemson.edu>
2020-09-23 21:57:11 -0400

    test screen recording

--- screen_test.py
+++ screen_test.py
@@ -1,35 +1,26 @@
 from grabscreen import grab_screen
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import math
-import pygame
-import pyvjoy
-from getKeys import key_check
-import pyautogui
-import numpy as np
-import os
 import cv2
-import time
 
-print("code starts in..")
-time.sleep(0.5)
-for i in range(0, 3):
-    k = 3 - i
-    print("{}".format(k))
-    time.sleep(1)
-pause = False
+"""
+    > This code test the screen recording 
+    > Place the "800 x 600" GTA V game window on the top right corner of your screen
+    > Note that the screen should be of the resolution 1920 X 1080
+    > Correct recording should return the entire GTA V window with the sky region cropped out
+    > In case of incorrect screen recording change the recording region accordingly
+"""
+print("Press 'q' to quit recording")
 while True:
-    if not pause:
-        screen = grab_screen(region=(1170, 290, 1870, 430))
-        screen = cv2.resize(screen, (500, 400))
-        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
-        # screen = cv2.cvtColor(screen, cv2.COLOR_RGB2YUV)
-        screen = screen / 255
-        cv2.imshow('screen', screen)
-        screen = cv2.resize(screen, (100, 100))
+    """
+    The recording coordinates are in the form (x1, y1, x2, y2) where x1, y1 represent the top left corner and x2, y2 
+    represent the bottom right corner
+    """
 
-        print(screen.shape)
-        
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+    screen = grab_screen(region=(1170, 290, 1870, 430))
+    screen = cv2.resize(screen, (500, 400))
+    screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
+    cv2.imshow('screen', screen)
+    screen = cv2.resize(screen, (100, 100))
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
